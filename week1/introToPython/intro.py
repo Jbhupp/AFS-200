57697590e7a8bacfb3d9c6d0c8d764a04efde157
github_Jbhupp <jenni.hupp@bryanuniversity.edu>
2023-03-25 16:28:47 -0400

    commit

---
+++ week1/introToPython/intro.py
@@ -0,0 +1,15 @@
+name = input("Give me your name: ")
+print("Your name is " + name)
+
+age = int(input("Enter your age: "))
+
+
+print("Were" + "wolf")
+
+print("Door" + "man")
+
+print("4" + "chan")
+
+print(str(4) + "chan")
+
+print(4 * "test")
