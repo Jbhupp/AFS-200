a26ed76e235cbce47684300633ddef137a5e226e
github_Jbhupp <jenni.hupp@bryanuniversity.edu>
2023-03-25 16:21:16 -0400

    commit

---
+++ week1/assignment1/apples.py
@@ -0,0 +1,15 @@
+applePrice = 0.50
+
+name = input("What is your name?")
+
+#print("Hello " + name)
+
+message = int(input(f'Hello {name}, Apples cost ${applePrice:,.2f} each. How many would you like?'))
+
+#print(message)
+
+thanks = f"Thank you for your purchase of {message} apples at ${applePrice:,.2f} each. Your total today is ${(message * applePrice):,.2f}"
+
+print(thanks)
+
+
