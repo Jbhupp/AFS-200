b9ffb41f8fcf5b816b1bc5b2948bcce77baec04b
github_Jbhupp <jenni.hupp@bryanuniversity.edu>
2023-03-25 16:43:40 -0400

    commit

---
+++ week2/magic.py
@@ -0,0 +1,14 @@
+# Prompt the user to input a number.
+
+message = int(input('Please enter a number: '))
+
+# Multiple this number by 3.
+# Add 6 to the number
+# Divide the new number by 3.
+# Subtract the number from step 1 from the answer in step 4.
+
+msg= ((((message * 3) + 6) // 3) - message)
+
+# Display the results as an integer.  The results should always be 2. 
+
+print(msg)
